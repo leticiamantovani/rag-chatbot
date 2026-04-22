78301f489620c5a660876bf66670027fe1c5a9cc
Leticia Mantovani <leticiamantovani159@gmail.com>
2026-04-22 11:35:46 -0300

    feat: add streaming

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
 from app.services.chat_service import chat_service
-from app.schema.chat import ChatRequest, ChatResponse
+from app.schema.chat import ChatRequest
 
 router = APIRouter()
 
-@router.post("/chat", response_model=ChatResponse)
+@router.post("/chat", response_class=StreamingResponse)
 def get_answer(request: ChatRequest):
-    return ChatResponse(answer=chat_service(request.question, request.collection_name))
+    return StreamingResponse(chat_service(request.question, request.collection_name), media_type="text/plain")
--- app/schema/chat.py
+++ app/schema/chat.py
@@ -1,8 +1,5 @@
 from pydantic import BaseModel
 
-class ChatResponse(BaseModel):
-    answer: str
-
 class ChatRequest(BaseModel):
     question: str
     collection_name: str
