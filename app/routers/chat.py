af05d9197b027a2d6db6ecf89ab29408d0a13860
Leticia Mantovani <leticiamantovani159@gmail.com>
2026-04-26 12:24:28 -0300

    feat: add chat history on chat API

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
-from app.services.chat_service import chat_service
+from app.services.chat_service import chat_service, resolve_conversation
 from app.schema.chat import ChatRequest
+from app.core.dependencies import get_db
+from sqlalchemy.orm import Session
+from uuid import UUID
 
-router = APIRouter()
+router = APIRouter(prefix="/chat", tags=["chat"])
 
-@router.post("/chat", response_class=StreamingResponse)
-def get_answer(request: ChatRequest):
-    return StreamingResponse(chat_service(request.question, request.collection_name), media_type="text/plain")
+@router.post("", response_class=StreamingResponse)
+def get_answer(request: ChatRequest, db: Session = Depends(get_db), conversation_id: UUID | None = None):
+    conversation = resolve_conversation(db, conversation_id)
+
+    response = chat_service(db, conversation, request.question, request.collection_name)
+
+    return StreamingResponse(response, media_type="text/plain", headers={"X-Conversation-ID": str(conversation.id)})
--- app/services/chat_service.py
+++ app/services/chat_service.py
@@ -1,25 +1,52 @@
-from langchain_ollama import OllamaEmbeddings
-from langchain_postgres import PGVector
-from langchain.chat_models import init_chat_model
 from app.core.config import get_vector_store
 from app.core.dependencies import create_embeddings, create_model
+from sqlalchemy.orm import Session
+from app.db.models.messages import Message
+from app.db.models.conversations import Conversation
+from fastapi import HTTPException
+from uuid import UUID
 
 embeddings = create_embeddings()
 model = create_model()
 
 
-def chat_service(question: str, collection_name: str):
+def chat_service(db: Session, conversation: Conversation, question: str, collection_name: str):
+    db.add(Message(conversation_id=conversation.id, content=question, role="user"))
+    db.commit()
+
+    context = get_context(collection_name, question)
+    return generate_response(db, conversation, context, question)
+
+
+def generate_response(db: Session, conversation: Conversation, context: str, question: str):
+    buffer = []
+    prompt = f"Use the context below to answer the question.\n\nContext:\n{context}\n\nQuestion: {question}"
+    try: 
+        for chunk in model.stream(prompt):
+            buffer.append(chunk.content)
+            yield chunk.content
+    finally:
+        db.add(Message(conversation_id=conversation.id, content="".join(buffer), role="assistant"))
+        db.commit()
+
+
+def get_context(collection_name: str, question: str):
     question_embedding = embeddings.embed_query(question)
     vector_store = get_vector_store(embeddings, collection_name)
-
     results = vector_store.similarity_search_by_vector(
         question_embedding,
         k=2,
     )
-
     context = "\n\n".join([doc.page_content for doc in results])
-    prompt = f"Use the context below to answer the question.\n\nContext:\n{context}\n\nQuestion: {question}"
+    return context
 
-    for chunk in model.stream(prompt):
-        yield chunk.content
-    
+def resolve_conversation(db: Session, conversation_id: UUID | None = None) -> Conversation:
+    if conversation_id:
+        conversation = db.get(Conversation, conversation_id)
+        if not conversation:
+            raise
+        return conversation
+    conversation = Conversation()
+    db.add(conversation)
+    db.flush()
+    return conversation
